378dfda02711e617c41ac05b319b4c51f47c913f
AyAl02 <ayham-20021@hotmail.com>
2025-11-19 12:35:47 +0100

    Delete CryptoInfo.cs

--- CryptoInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GeoInsight.Models
-{
-    public class CryptoInfo
-    {
-        public decimal BitcoinPrice { get; set; }  
-        public decimal EthereumPrice { get; set; }
-        public string VsCurrency { get; set; } = "usd";
-    }
-}
